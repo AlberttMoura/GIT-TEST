dd86147a51b80952e7aceda2832325d0e28d48b6
Alberto Moura <56458241+AlberttMoura@users.noreply.github.com>
2021-12-02 16:32:44 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 
+console.log('test')
 async function getCep(cep) {
     axios.get(`https://viacep.com.br/ws/${cep}/json/`).then((response) => {
         console.log(response.data)
